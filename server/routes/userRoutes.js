9469f25368c7cd2129cc3d20e9254f1922de41db
Wahid ali <wahidali3150@gmail.com>
2026-03-11 04:00:10 +0500

    Routes created for user singup, signin & check auth

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { registerUser, loginUser } = require("../controllers/authController");
+const authenticate = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/check-auth", authenticate, (req, res) => {
+  const user = req.user;
+
+  res.status(200).json({
+    success: true,
+    message: "Authenticated user!",
+    data: {
+      user,
+    },
+  });
+});
+
+module.exports = router;
