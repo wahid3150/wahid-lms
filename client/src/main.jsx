105ff248e16a720a9d5f246e5b0594499f24a786
Wahid ali <wahidali3150@gmail.com>
2026-03-16 02:58:24 +0500

    add app shell and routing setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/auth-context/index.jsx";
+import InstructorProvider from "./context/instructor-context/index.jsx";
+import StudentProvider from "./context/student-context/index.jsx";
+import { Toaster } from "./components/ui/sonner.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <InstructorProvider>
+        <StudentProvider>
+          <App />
+          <Toaster richColors position="top-right" />
+        </StudentProvider>
+      </InstructorProvider>
+    </AuthProvider>
+  </BrowserRouter>,
+);
