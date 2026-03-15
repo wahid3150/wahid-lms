a71762e9f640c013f8c1e9a1c0b09727ead82035
Wahid ali <wahidali3150@gmail.com>
2026-03-16 03:03:47 +0500

    add student and instructor pages

---
+++ client/src/pages/instructor/index.jsx
@@ -0,0 +1,115 @@
+import InstructorCourses from "@/components/instructor-view/courses";
+import InstructorDashboard from "@/components/instructor-view/dashboard";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
+import { AuthContext } from "@/context/auth-context";
+import { InstructorContext } from "@/context/instructor-context";
+import {
+  courseCurriculumInitialFormData,
+  courseLandingInitialFormData,
+} from "@/config";
+import { fetchInstructorCourseListService } from "@/services";
+import { BarChart, Book, LogOut } from "lucide-react";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function InstructorDashboardpage() {
+  const [activeTab, setActiveTab] = useState("courses");
+  const { resetCredentials } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const {
+    instructorCoursesList,
+    setInstructorCoursesList,
+    setCurrentEditedCourseId,
+    setCourseLandingFormData,
+    setCourseCurriculumFormData,
+  } = useContext(InstructorContext);
+
+  async function fetchAllCourses() {
+    const response = await fetchInstructorCourseListService();
+    if (response?.success) setInstructorCoursesList(response?.data);
+  }
+
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
+
+  const menuItems = [
+    {
+      icon: BarChart,
+      label: "Dashboard",
+      value: "dashboard",
+      component: <InstructorDashboard listOfCourses={instructorCoursesList} />,
+    },
+    {
+      icon: Book,
+      label: "Courses",
+      value: "courses",
+      component: <InstructorCourses listOfCourses={instructorCoursesList} />,
+    },
+    {
+      icon: LogOut,
+      label: "Logout",
+      value: "logout",
+      component: null,
+    },
+  ];
+
+  function handleLogout() {
+    resetCredentials();
+    sessionStorage.clear();
+  }
+
+  function handleCreateCourse() {
+    setCurrentEditedCourseId(null);
+    setCourseLandingFormData(courseLandingInitialFormData);
+    setCourseCurriculumFormData(courseCurriculumInitialFormData);
+    navigate("/instructor/create-new-course");
+  }
+
+  return (
+    <div className="flex h-full min-h-screen bg-gray-100">
+      <aside className="w-64 bg-white shadow-md hidden md:block">
+        <div className="p-4">
+          <h2 className="text-2xl font-bold mb-4">Instructor View</h2>
+          <nav>
+            {menuItems.map((menuItem) => (
+              <Button
+                className="w-full justify-start mb-2"
+                key={menuItem.value}
+                variant={activeTab === menuItem.value ? "secondary" : "ghost"}
+                onClick={
+                  menuItem.value === "logout"
+                    ? handleLogout
+                    : () => setActiveTab(menuItem.value)
+                }
+              >
+                <menuItem.icon className="mr-2 h-4 w-4" />
+                {menuItem.label}
+              </Button>
+            ))}
+          </nav>
+        </div>
+      </aside>
+      <main className="flex-1 p-8 overflow-y-auto">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex items-center justify-between mb-8 gap-4">
+            <h1 className="text-3xl font-bold">
+              {activeTab === "courses" ? "Courses" : "Dashboard"}
+            </h1>
+            <Button onClick={handleCreateCourse}>Create New Course</Button>
+          </div>
+          <Tabs value={activeTab} onValueChange={setActiveTab}>
+            {menuItems.map((menuItem) => (
+              <TabsContent key={menuItem.value} value={menuItem.value}>
+                {menuItem.component !== null ? menuItem.component : null}
+              </TabsContent>
+            ))}
+          </Tabs>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default InstructorDashboardpage;
